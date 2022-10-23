5fa33a7613a0259a6a10e17771d6613a99eeb452
Sir-Lord <lwiafe1@student.gsu.edu>
2022-10-23 16:05:25 -0400

    feat: unauthorized error class

---
+++ FoodOasis-server/src/common/exceptions/UnauthorizedError.ts
@@ -0,0 +1,13 @@
+import { ExceptionCore } from "@core//";
+import { ErrorType } from "@utils//";
+
+/**
+ * @class Unauthorized Error Exception
+ * @extends { ExceptionCore } which extends JS Error base class
+ */
+ export default class UnauthorizedError extends ExceptionCore {
+    constructor(message = 'Unauthorized User') {
+        super(ErrorType.UNAUTHORIZED, message);
+        return super.handle(this)
+    }
+}
