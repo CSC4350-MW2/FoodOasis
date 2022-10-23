b9673f38e39159ea01c0f09397b72a559127f009
Sir-Lord <lwiafe1@student.gsu.edu>
2022-10-23 16:00:46 -0400

    feat: forbidden error class

---
+++ FoodOasis-server/src/common/exceptions/ForbiddenError.ts
@@ -0,0 +1,13 @@
+import { ExceptionCore } from "@core//";
+import { ErrorType } from "@utils//";
+
+/**
+ * @class Forbidden Error Exception
+ * @extends { ExceptionCore } which extends JS Error base class
+ */
+export default class ForbiddenError extends ExceptionCore {
+    constructor(message = 'Permission denied') {
+        super(ErrorType.FORBIDDEN, message);
+        return super.handle(this)
+    }
+}
