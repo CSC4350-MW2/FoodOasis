f7863a9c814f9478bc2eba703a59a9120f1351a3
Sir-Lord <lwiafe1@student.gsu.edu>
2022-10-23 16:01:38 -0400

    feat: internal server error class

---
+++ FoodOasis-server/src/common/exceptions/InternalServerError.ts
@@ -0,0 +1,13 @@
+import { ExceptionCore } from "@core//";
+import { ErrorType } from "@utils//";
+
+/**
+ * @class Internal Error Exception
+ * @extends { ExceptionCore } which extends JS Error base class
+ */
+export default class InternalServerError extends ExceptionCore {
+    constructor(message = 'Internal error', error?: string) {
+        super(ErrorType.INTERNAL, message, error);
+        return super.handle(this)
+    }
+}
