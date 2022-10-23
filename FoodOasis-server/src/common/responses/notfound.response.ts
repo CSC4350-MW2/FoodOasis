edabb31f1678e9bfd709c10e9b294873f6899e95
Sir-Lord <lwiafe1@student.gsu.edu>
2022-10-23 15:21:52 -0400

    feat: NotFound response class

---
+++ FoodOasis-server/src/common/responses/notfound.response.ts
@@ -0,0 +1,20 @@
+import { ApiResponse } from "@core/response.core";
+import { StatusCode, Success, ErrorType } from "@utils/";
+import { response } from 'express';
+
+/**
+ * @class sends not found response to client
+ */
+export class NotFoundResponse extends ApiResponse {
+    private url: string | undefined;
+
+    constructor(message = 'Not Found') {
+        super();
+        this.url = response.req?.originalUrl;
+        this.success = Success.ERROR;
+        this.status_code = StatusCode.NOTFOUND;
+        this.message = message;
+        this.error = ErrorType.NOTFOUND;
+        this.prepare<NotFoundResponse>(this);
+    }
+}
