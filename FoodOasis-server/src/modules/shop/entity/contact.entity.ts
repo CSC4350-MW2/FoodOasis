c71ade6eaccc1ee5ea193fdec4161d4c0c98f21e
sidehenhub <67240582+sidehenhub@users.noreply.github.com>
2022-11-09 20:15:15 -0500

    feat: contact database entity

---
+++ FoodOasis-server/src/modules/shop/entity/contact.entity.ts
@@ -0,0 +1,15 @@
+import { EntityCore } from "@server/common/core/entity.core";
+import { Column, Entity } from "typeorm";
+import { IContact } from "../interface/contact.interface";
+
+@Entity()
+export class ContactEntity extends EntityCore<IContact> implements IContact{ //export the class allows it to be accessible outside
+    @Column("int")
+    phone:number;
+
+    @Column("varchar")
+    social:string;
+    
+    @Column("varchar")
+    website:string;
+}
