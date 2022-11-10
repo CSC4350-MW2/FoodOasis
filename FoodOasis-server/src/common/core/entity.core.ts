c4fe249b6f5770c0c645cd5172a0e1d35552212d
sidehenhub <67240582+sidehenhub@users.noreply.github.com>
2022-11-09 19:56:12 -0500

    feat: entity core class

---
+++ FoodOasis-server/src/common/core/entity.core.ts
@@ -0,0 +1,23 @@
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BeforeUpdate } from 'typeorm';
+
+export class EntityCore<T> {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: 'timestamptz' })
+    updatedAt!: Date;
+
+    constructor(input?: Partial<T>) {
+      if (input) {
+        Object.assign(this, input);
+      }
+    }
+
+    @BeforeUpdate()
+    updateDate() {
+      this.updatedAt = new Date();
+    }
+}
