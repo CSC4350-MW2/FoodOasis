5e70eb4533450e934ceb1b5024e49a61f66928e1
Sir-Lord <lwiafe1@student.gsu.edu>
2022-11-21 18:54:08 -0500

    chore: adding logging middleware for API requests

---
+++ FoodOasis-server/src/common/middlewares/log.middleware.ts
@@ -0,0 +1,24 @@
+import morgan from 'morgan'
+import { Service } from 'typedi';
+import { NextFunction, Request, Response } from "express";
+import { ExpressMiddlewareInterface, Middleware } from "routing-controllers";
+import { LoggerService } from '@logger//';
+import { AppConfig } from '@config//';
+
+
+
+@Service()
+@Middleware({ type: 'before' })
+export class LogMiddleware implements ExpressMiddlewareInterface {
+
+    private log = new LoggerService("API request");
+
+    public use(req: Request, res: Response, next: NextFunction): any {
+        const skip = () => { return AppConfig.env === "development" ? false : true; };
+
+        return morgan('dev', {
+            stream: { write: this.log.http.bind(this.log) },
+            skip
+        })(req, res, next);
+    }
+}
