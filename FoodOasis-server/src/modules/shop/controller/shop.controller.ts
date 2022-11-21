41e0a7a41374feb7ee020cf3445cc5fb28321d8f
Sir-Lord <lwiafe1@student.gsu.edu>
2022-11-20 23:25:46 -0500

    feat: update shop

--- FoodOasis-server/src/modules/shop/controller/shop.controller.ts
+++ FoodOasis-server/src/modules/shop/controller/shop.controller.ts
@@ -1,10 +1,10 @@
 import { Service } from 'typedi'
 import { ResponseSchema } from 'routing-controllers-openapi';
-import { Controller, Body, Post, Get, CurrentUser, Authorized } from 'routing-controllers';
+import { Controller, Body, Post, Get, CurrentUser, Authorized, Put } from 'routing-controllers';
 
 import { SuccessResponse } from '@responses//';
 import { ShopService } from '../services/shop.service';
-import { CreateShopDto } from '../dto/shop.dto';
+import { CreateShopDto, UpdateShopDto } from '../dto/shop.dto';
 import { ShopData, ShopResponse } from '../dto/responses/shop.response';
 
 @Service()
@@ -18,15 +18,23 @@ export class UserController {
     @Authorized()
     @ResponseSchema(ShopResponse)
     async createShop(@Body() body: CreateShopDto, @CurrentUser() {userId}: CurrentUser): Promise<ShopResponse> {
-        const shop = await this.shopService.createShop({ userId, ...body })
-        return new SuccessResponse<ShopData>('Shop Created', { shop })
+        const shop = await this.shopService.createShop({ userId, ...body });
+        return new SuccessResponse<ShopData>('Shop Created', { shop });
     }
 
     @Get('/:id')
     @Authorized()
     @ResponseSchema(ShopResponse)
     async getProfile(@CurrentUser() {userId}: CurrentUser ): Promise<ShopResponse> {
-        const shop = await this.shopService.getShop({ userId })
-        return new SuccessResponse<ShopData>('Shop Found', { shop })
+        const shop = await this.shopService.getShop({ userId });
+        return new SuccessResponse<ShopData>('Shop Found', { shop });
+    }
+
+    @Put('/:id')
+    @Authorized()
+    @ResponseSchema(ShopResponse)
+    async updateProfile(@Body() body: UpdateShopDto, @CurrentUser() {userId}: CurrentUser): Promise<ShopResponse> {
+        const shop = await this.shopService.update({ userId }, body);
+        return new SuccessResponse<ShopData>('Updated Profile', { shop });
     }
 }
