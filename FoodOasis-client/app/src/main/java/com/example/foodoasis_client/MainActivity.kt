2bb953b026e5659cf15513c75330cee865aa5d80
Sir-Lord <lwiafe1@student.gsu.edu>
2022-11-25 18:51:23 -0500

    refactor: MVVM architecture for listing shops feature

--- FoodOasis-client/app/src/main/java/com/example/foodoasis_client/MainActivity.kt
+++ FoodOasis-client/app/src/main/java/com/example/foodoasis_client/MainActivity.kt
@@ -4,72 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodoasis_client.adapter.ShopsAdapter
-import com.example.foodoasis_client.model.auth.AuthUser
-import com.example.foodoasis_client.model.shops.ShopsList
-import com.example.foodoasis_client.service.AuthService
-import com.example.foodoasis_client.service.ServiceBuilder
-import com.example.foodoasis_client.service.ShopService
+import com.example.foodoasis_client.model.shops.Shop
+import com.example.foodoasis_client.viewModel.ShopViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.HttpException
-import retrofit2.Response
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var shopsAdapter: ShopsAdapter
+    private lateinit var shopViewModel: ShopViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val context = this
-
-        val destinationService = ServiceBuilder.buildService(ShopService::class.java)
-        val requestCall = destinationService.getShops()
-
-        requestCall.enqueue(object: Callback<ShopsList> {
-            override fun onResponse(call: Call<ShopsList>, response: Response<ShopsList>) {
-                val body = response.body()!!
-                if (response.isSuccessful) {
-                    shopsAdapter = ShopsAdapter(body.data.shops)
-
-                    rvShopItems.adapter = shopsAdapter
-                    rvShopItems.layoutManager = LinearLayoutManager(context)
-
-                } else {
-                    try {
-                        val jObjError = JSONObject(response.errorBody()!!.charStream().readText())
-                        Toast.makeText(
-                            context,
-                            jObjError.getString("message"),
-                            Toast.LENGTH_LONG
-                        ).show()
-                    } catch (e: Exception) {
-                        Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-
-
-            override fun onFailure(call: Call<ShopsList>, t: Throwable) {
-                val errorMessage = when (t) {
-                    is IOException -> "No internet connection"
-                    is HttpException -> "Something went wrong!"
-                    else -> t.localizedMessage
-                }
-                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
-            }
-        })
+        loadShops()
 
         val login = findViewById<Button>(R.id.loginButtonHome)
         val signup = findViewById<Button>(R.id.signupButtonHome)
         login.setOnClickListener { startActivity(Intent(this, LoginActivity::class.java)) }
         signup.setOnClickListener { startActivity(Intent(this, SignupActivity::class.java)) }
+    }
+
+    private fun loadShops(){
+        shopViewModel = ViewModelProvider(this)[ShopViewModel::class.java]
+        shopViewModel.getShopsData(this)
+        shopViewModel.shopDataList.observe(this) {
+            initShopsAdapter(it)
+        }
+    }
+
+    private fun initShopsAdapter(data:List<Shop>){
+        rvShopItems.layoutManager = LinearLayoutManager(this)
+        rvShopItems.adapter = ShopsAdapter(data)
     }
 }
